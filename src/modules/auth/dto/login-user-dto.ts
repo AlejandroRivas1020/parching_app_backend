db6e141c46fdc3516c71b5fd6ae6370a027db612
AlejandroRivas <a.rivaslopez020@gmail.com>
2024-09-16 21:53:54 -0500

    Documentation added in swagger for auth and users

--- src/modules/auth/dto/login-user-dto.ts
+++ src/modules/auth/dto/login-user-dto.ts
@@ -1,9 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'user@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description:
+      'The password for the user. Must be at least 8 characters long.',
+    example: 'password123',
+  })
   @IsString()
   @MinLength(8)
   password: string;
--- src/modules/auth/dto/register-user-dto.ts
+++ src/modules/auth/dto/register-user-dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -8,29 +9,62 @@ import {
 import { Gender } from '../../client/enums/gender.enum';
 
 export class RegisterUserDto {
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'John Doe',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'user@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description:
+      'The password for the user. Must be at least 8 characters long.',
+    example: 'password123',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    description: "URL or base64 encoded string of the user's profile picture",
+    example: 'https://example.com/profile.jpg',
+  })
   @IsString()
   profilePicture: string;
 
+  @ApiProperty({
+    description: 'Address of the user',
+    example: '123 Main St, Anytown, USA',
+  })
   @IsString()
   address: string;
 
+  @ApiProperty({
+    description: 'The birth date of the user in ISO 8601 format',
+    example: '1990-01-01',
+  })
   @IsDateString()
   birthDate: string;
 
+  @ApiProperty({
+    description: 'Gender of the user',
+    enum: Gender,
+  })
   @IsEnum(Gender)
   gender: Gender;
 
+  @ApiProperty({
+    description: "Description of the user's location",
+    example: 'Lives near the central park',
+  })
   @IsString()
   locationDescription: string;
 }
